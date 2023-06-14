b936c2e5c592499c07b3ee2dcddb0db5c1b7a3c7
ToS4 <ayomohamad1@gmail.com>
2023-06-14 20:46:55 +0200

    hide hitboxes

--- src/LightningMage.py
+++ src/LightningMage.py
@@ -296,7 +296,7 @@ class LightningMage():
             @param SCREEN   the pygame screen object
             """
         
-        pygame.draw.rect(SCREEN, (0,255,0), self.rect)
+        #pygame.draw.rect(SCREEN, (0,255,0), self.rect)
         img = pygame.transform.scale(pygame.transform.flip(self.image, self.flip, False), (self.width_hitbox*3.5*self.width_factor, self.height_hitbox*2*self.height_factor))
         img_rect = img.get_rect()
         if not self.flip:
